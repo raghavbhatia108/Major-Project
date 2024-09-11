50cfdcb8106dd918dc5e6e08e3e82e22b5a1b89d
Raghav bhatia <bhatiaraghav70@gmail.com>
2024-09-11 18:53:03 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_env!="production"){
-  require('dotenv').config()
+if (process.env.NODE_env != "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -7,7 +7,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const dbUrl = process.env.ATLASDB_URL;
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -22,22 +22,22 @@ const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
 
 const store = MongoStore.create({
-  mongoUrl:dbUrl,
-  crypto:{
-    secret:process.env.SECRET,
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
   },
-  touchAfter:24*3600,
+  touchAfter: 24 * 3600,
 });
 
-store.on("error", ()=>{
+store.on("error", () => {
   console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
   store,
-  secret:process.env.SECRET,
-  resave:false,
-  saveUninitialized:false,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
   rolling: true,
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
@@ -46,10 +46,18 @@ const sessionOptions = {
   },
 };
 
-
-
 app.use(session(sessionOptions));
 app.use(flash());
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 app.use(passport.initialize());
@@ -70,28 +78,16 @@ passport.deserializeUser(User.deserializeUser());
 
 main().catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect(dbUrl);
-}
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
-
-
-
-
-app.get("/demouser", async (req,res)=>{
-let fakeUser = new User({
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
     email: "student@gmail.com",
-    username : "delta-student",
-});
+    username: "delta-student",
+  });
 
-let registeredUser = await User.register(fakeUser, "HelloWorld");
-res.send(registeredUser);
-})
+  let registeredUser = await User.register(fakeUser, "HelloWorld");
+  res.send(registeredUser);
+});
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
@@ -103,7 +99,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Somethhing Went Wrong" } = err;
-  res.status(statusCode).send(message);
+  // res.status(statusCode).send(message);
   res.render("error.ejs", { message });
 });
 
